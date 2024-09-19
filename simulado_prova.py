613e47de6349e0d426c76322816671d801b4fe1c
Leonardo Portella <leonardo.portella.2005@gmail.com>
2024-09-19 09:09:57 -0300

    simulado

--- simulado_prova.py
+++ simulado_prova.py
@@ -4,10 +4,22 @@ tipos_cores = {
     'secundaria': ['verde', 'laranja', 'roxo'],
     }
 
+for key, value in tipos_cores.items():
+    print(value)
+for key, value in tipos_cores.items():
+    for cor in value:
+        print(cor)
+
 
 # 2 - Faca um for loop na lista e mostre qual tipo ela é
 lst_cores = ['vermelho', 'roxo', 'preto'] 
-
+for cor in lst_cores:
+    if cor in tipos_cores['primaria']:
+        print(f"{cor} é primaria")
+    elif cor in tipos_cores['secundaria']:
+        print(f"{cor} é secundaria")
+    else:
+        print(f"{cor} é neutra")
 
 
 
@@ -23,6 +35,14 @@ precos = {
     }
 
 compra = ['luva', 'blusa', 'tenis']
+valor_compra = precos['luva'] + precos['blusa'] + precos['tenis']
+print(valor_compra)
+
+total_compra = 0
+for item in compra:
+    preco = precos[item]
+    total_compra = total_compra + preco
+print(f"O total da compra é {total_compra}")
 
 
 # 4 – Faca uma funcao que entre 3 notas e calcule a media final de acordo com o crirterio de nota do ibmec
@@ -31,3 +51,6 @@ compra = ['luva', 'blusa', 'tenis']
 p1 = 8.5
 p2 = 9.0
 ac = 1.0
+from meu_pacote.modulo import calcular_nota
+nota_final = calcular_nota(p1, p2, ac)
+print(f"A nota final foi {nota_final}")
